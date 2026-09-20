faff71e8a5ae9367de8f567115f997d20fd55899
Ananya <ananyasingh142006@gmail.com>
2026-09-20 19:11:50 +0530

    updated project files

--- backend/Lab3/httpserver.js
+++ backend/Lab3/httpserver.js
@@ -1,16 +1,82 @@
-import http from 'http';
+import http from "http";
+import fs from "node:fs/promises";
 
-const server = http.createServer((req, res) => {
-    console.log("URL:", req.url);
-    console.log("Method:", req.method);
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end(`Hello, World!`);
+let port = 3001;
+const filePath = "file.txt";
+
+let userData = [];
+
+async function readFile() {
+    try {
+        const data = await fs.readFile(filePath, "utf-8");
+        return data;
+    } catch (err) {
+        console.log("Error found", err);
+        return "";
+    }
+}
+
+const content = await readFile();
+
+console.log(content, "12");
+
+const server = http.createServer((req, resp) => {
+
+    const url = req.url;
+    const typ = req.method;
+
+    
+    if (url === "/msg" && typ === "GET") {
+
+        resp.statusCode = 200;
+        resp.setHeader("Content-Type", "application/json");
+
+        resp.end(JSON.stringify(content));
+    }
+
+    
+    else if (url === "/sis" && typ === "GET") {
+
+        resp.statusCode = 200;
+        resp.setHeader("Content-Type", "application/json");
+
+        const user = {
+            name: "aarav",
+            id: 1223
+        };
+
+        resp.end(JSON.stringify(user));
+    }
+
+    
+    else if (url === "/create" && typ === "POST") {
+        let body = "";
+        req.on("data", (chunk) => {
+            body += chunk;
+        });
+        req.on("end", () => {
+                const data = JSON.parse(body);
+                const newUser = {
+                    id: data.id,
+                    name: data.name,
+                    age: data.age
+                };
+                userData.push(newUser);
+                resp.end(JSON.stringify({message: "User created successfully"}));
+            } 
+        )
+        }
+    else if(url=="/user" && typ=="GET"){
+        resp.end(JSON.stringify(userData));
+    }
+    
+    else {
+
+        resp.statusCode = 404;
+        resp.end("Route not found");
+    }
 });
 
-const port = 7000;
 server.listen(port, () => {
-  
-    const message = `Server is running on http://localhost:${port}/`;
-    console.log(message);
- 
+    console.log(`Server is running on port ${port}`);
 });
